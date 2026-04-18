5df4145ab543ae7c92010b33731fdcf75d346eaa
vijay <vijay@supporthive.com>
2026-04-18 19:07:14 +0530

    structure ready header and footer

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Brandpack
-https://www.figma.com/design/690IrqqDz13qloEkNWhADo/AD534---Reskin--E-Sim----Simseekr.com?node-id=3357-851&t=my6hh3G3Wd1vewSj-1
+# Simseekr
+
+Start Date : 17th april
+End Date : 25th april
+
+QA - Parth
+
+
+Developer Team:- Vijay,Mehul,Rahul,Dhiraj,Trithak,Medhnash
+Project Report : Jeet 
+Project Lead : Vijay
+
+Vijay:Header,Footer,
+Rahul: home page, checkout page, checkout-success, checkout-error same page (pop)
+Dhiraj: Plan-page,Plan-individual,Plans-Regional, Plan-individual-Regional, same page (pop)
+Trithak: Aboutus-page,Account page all, same page (pop)
+Medhnash:Privacy,Terms,Compatibility,error page, same page (pop)
+saakshi c : login, reset password,reset password request  ,register,register(error) ,404 error , same page (pop)
+saakshi k : help-center, contactus, contactus-succesful, contactus-error, same page (pop)
+
+
+figma url : https://www.figma.com/design/690IrqqDz13qloEkNWhADo/AD534---Reskin--E-Sim----Simseekr.com?node-id=11206-110278&t=69h8ozdvRWavGc1x-0
+
+
+prototype link :https://www.figma.com/proto/690IrqqDz13qloEkNWhADo/AD534---Reskin--E-Sim----Simseekr.com?node-id=8256-2700&t=69h8ozdvRWavGc1x-0&scaling=min-zoom&content-scaling=fixed&page-id=3357%3A851
+
---
+++ includes/header.php
@@ -0,0 +1,99 @@
+<?php
+  $currentPage = basename($_SERVER['PHP_SELF']);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simseekr</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="./img/tg-icon.svg">
+    <link rel="stylesheet" href="uiframe/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.css">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="uiframe/css/slick.css">
+    <link rel="stylesheet" href="uiframe/css/slick-theme.css">
+    <link rel="stylesheet" href="css/t_styles.css">
+    <link rel="stylesheet" href="css/rstyle.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="uiframe/css/font-awesome.min.css">
+    <link rel="stylesheet" href="uiframe/css/animate.min.css">
+    <link href="uiframe/css/aos.css" rel="stylesheet">
+  </head>
+  <body>
+  
+  <div class="main-div">
+    <header class="header-top fixed-top" id="header-top">
+      <nav class="navbar navbar-expand-lg">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              <img src="./img/hlogo.svg" alt="Simseekr Logo" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav m-auto">
+                  <h5>Navigation</h5>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Shop eSIMs</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">Support Hub</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">Contact Us</a>
+                  </li>
+                </ul>
+                <ul class="navbar-nav m-auto menu-items-mobile">
+                  <h5>Navigation</h5>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Terms & Conditions</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Privacy Policy</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">Cookies Policy</a>
+                  </li>
+                </ul>
+                <div class="d-flex right-side-menu">
+                  <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                      <button class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      $ USD <img src="./img/usa-flag.svg" class="img-fluid">
+                      </button>
+                      <ul class="dropdown-menu">
+                        <li><button class="dropdown-item" onclick="selectCurrency('USD')">$ USD <img src="./img/usa-flag.svg" class="img-fluid"></button></li>
+                        <li><button class="dropdown-item" onclick="selectCurrency('CAD')">$ CAD <img src="./img/usa-flag.svg" class="img-fluid"></button></li>
+                        <li><button class="dropdown-item" onclick="selectCurrency('EUR')">$ EUR <img src="./img/usa-flag.svg" class="img-fluid"></button></li>
+                        <li><button class="dropdown-item" onclick="selectCurrency('GBP')">$ GBP <img src="./img/usa-flag.svg" class="img-fluid"></button></li>
+                        <li><button class="dropdown-item" onclick="selectCurrency('AUD')">$ AUD <img src="./img/usa-flag.svg" class="img-fluid"></button></li>
+                        <li><button class="dropdown-item" onclick="selectCurrency('NZD')">$ NZD <img src="./img/usa-flag.svg" class="img-fluid"></button></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link ">
+                        <img src="./img/shopping-basket-solid.svg" alt="Shopping Basket Icon" class="shopping-basket-icon img-fluid">
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <img src="./img/user-icon.svg" alt="Support Icon" class="user-icon img-fluid">
+                    </li>
+                  </ul>
+                </div> 
+                <div class="header-bottom d-block d-lg-none d-md-block">
+                    <p>© 2026 Simseekr. All rights reserved.</p>
+                </div>
+            </div>
+          </div>
+      </nav>
+   </header>
+  
+  
