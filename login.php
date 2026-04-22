fbf651993a97b8eba6a4550bccf4952493141759
vijay <vijay@supporthive.com>
2026-04-22 13:35:05 +0530

    header and footer

---
+++ login.php
@@ -0,0 +1,49 @@
+
+<?php include 'includes/header.php'; ?>
+
+<section class="auth-section auth-login">
+    <img src="./img/auth-bg.png" class="img-fluid d-none d-lg-block d-md-block bg-image">
+    <img src="./img/auth-bg-mo.png" class="img-fluid d-block d-lg-none d-md-none bg-image">
+    <div class="container">
+        <div class="container">
+            <div class="col">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="welcome-back">
+                            <h3>Welcome Back</h3>
+                            <form>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <input type="text" class="form-control" placeholder="Email address">
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="text" class="form-control" placeholder="Password">
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="forget-pass">
+                                            <a href="">Forgot Password?</a>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <button class="btn btn-green"> Login </button>
+                                    </div>
+                                    <div class="col-12">
+                                        <p>Don’t have an account? <a href="#">Register here</a></p>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php include 'includes/footer.php'; ?>
