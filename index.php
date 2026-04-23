2a58920f9c4f4bf2969d340dbca5bae98a157d78
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-04-23 06:04:51 +0530

    rahul

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php include 'includes/header.php'; ?>
+<!-- Home Section 1 Start -->
 <section>
 
 </section>
+<!-- Home Section 1 End -->
+<!-- Home Section 1 Start -->
+<section>
+
+</section>
+<!-- Home Section 1 End -->
+<!-- Home Section 1 Start -->
+<section>
+
+</section>
+<!-- Home Section 1 End -->
+<!-- Home Section 1 Start -->
+<section>
+
+</section>
+<!-- Home Section 1 End -->
 <?php include 'includes/footer.php'; ?>
