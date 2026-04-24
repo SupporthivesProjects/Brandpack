b853e23336019e94924f608a5bf3d66caae906d8
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-04-24 06:16:54 +0530

    rahul

---
+++ checkout_fail.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php'; ?>
+<!-- Success Section 1 Start -->
+<section class="success_s1">
+    <div class="success_c1 container">
+        <img class="success_icon" src="./img/fail_icon.svg">
+        <div class="success_titlebar">
+            <h1 class="success_title">Payment unsuccessful</h1>
+            <p class="success_subtitle mobile_none">We couldn’t complete your purchase. No charges were applied.<br>
+            Please try again or use a different payment method.</p>
+            <p class="success_subtitle desktop_none">We couldn’t complete your purchase. No charges were applied.
+            Please try again or use a different payment method.</p>
+        </div>
+        <div class="failed_btns">
+        <button type="button" class="btn fail_whitebtn">Return home</button>
+        <button type="button" class="btn fail_greenbtn">Try again</button>
+        </div>
+    </div>
+</section>
+<!-- Success Section 1 End -->
+<?php include 'includes/footer.php'; ?>
--- checkout_success.php
+++ checkout_success.php
@@ -1,7 +1,17 @@
 <?php include 'includes/header.php'; ?>
 <!-- Success Section 1 Start -->
-<section>
-
+<section class="success_s1">
+    <div class="success_c1 container">
+        <img class="success_icon" src="./img/success_icon.svg">
+        <div class="success_titlebar">
+            <h1 class="success_title">Your eSIM is ready</h1>
+            <p class="success_subtitle mobile_none">Your purchase was successful. Your eSIM is now available in your account and ready to install.<br>
+            Follow the quick setup steps to activate your connection and start using mobile data at your destination.</p>
+            <p class="success_subtitle desktop_none">Your purchase was successful. Your eSIM is now available in your account and ready to install.
+            Follow the quick setup steps to activate your connection and start using mobile data at your destination.</p>
+        </div>
+        <button type="button" class="btn curve_greenbtn"><img src="./img/curve_btnleft.svg"><p class="curvegreen_btntext">Install your eSIM</p><img src="./img/curve_btnright.svg"></button>
+    </div>
 </section>
 <!-- Success Section 1 End -->
 <?php include 'includes/footer.php'; ?>
