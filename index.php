d814673a7046cf24a2e4d29b62f7a00db2aa9bc1
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-04-24 12:52:07 +0530

    rahul

--- index.php
+++ index.php
@@ -4,19 +4,36 @@
 
 </section>
 <!-- Home Section 1 End -->
-<!-- Home Section 1 Start -->
-<section>
 
+<!-- Home Section 2 Start -->
+<section class="home_s2">
+    <div class="home_s1leftvector w-100">
+        <img class="home_s2v1 mobile_none" src="./img/home_s2v1.svg">
+        <img class="home_s2v2 mobile_none" src="./img/home_s2v2.svg">
+        <img class="home_s2v3 mobile_none" src="./img/home_s2v3.svg">
+    </div>
+    <div class="home_c2 w-100">
+        <h2 class="home_s2title">Reliable mobile data<br>for modern travel</h2>
+        <p class="home_s2subtitle"><span class="home_s2subtitlebig">Go Further and Stay Connected</span><br>Keep your connection with you wherever you go. No hunting for WiFi, no switching SIM cards, and no surprise roaming fees. Just dependable mobile data that helps you find your way, stay in touch, and make the most of every trip.<br><br>
+        <span class="home_s2subtitlebig">Instant Connection Across Borders</span><br>Our eSIM technology gives you fast access to mobile data without the need for a physical SIM. Choose your destination, set it up in moments, and connect to trusted networks around the world as soon as you arrive.</p>
+    </div>
+    <div class="home_s1rightvector w-100">
+        <img class="home_s2v4 mobile_none" src="./img/home_s2v4.svg">
+        <img class="home_s2v5 mobile_none" src="./img/home_s2v5.svg">
+        <img class="home_s2v6 mobile_none" src="./img/home_s2v6.svg">
+    </div>
 </section>
-<!-- Home Section 1 End -->
-<!-- Home Section 1 Start -->
-<section>
+<!-- Home Section 2 End -->
+ 
+<!-- Home Section 3 Start -->
+<!-- <section>
 
-</section>
-<!-- Home Section 1 End -->
-<!-- Home Section 1 Start -->
-<section>
+</section> -->
+<!-- Home Section 3 End -->
 
-</section>
-<!-- Home Section 1 End -->
+<!-- Home Section 4 Start -->
+<!-- <section>
+
+</section> -->
+<!-- Home Section 4 End -->
 <?php include 'includes/footer.php'; ?>
