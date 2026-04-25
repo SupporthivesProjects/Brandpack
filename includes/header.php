5106342bf362d642929c02f7b31ad01caa486dd1
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-04-25 15:02:49 +0530

    rahul

--- includes/header.php
+++ includes/header.php
@@ -16,9 +16,9 @@
     <link rel="stylesheet" href="uiframe/css/slick.css">
     <link rel="stylesheet" href="uiframe/css/slick-theme.css">
     <link rel="stylesheet" href="css/t_styles.css">
-    <link rel="stylesheet" href="css/rstyle.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/rstyle.css">
     <link rel="stylesheet" href="uiframe/css/font-awesome.min.css">
     <link rel="stylesheet" href="uiframe/css/animate.min.css">
     <link href="uiframe/css/aos.css" rel="stylesheet">
