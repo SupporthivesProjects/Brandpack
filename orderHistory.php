6b9543dee615596a77846eebbc71877b2ebdb452
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2026-04-27 13:15:45 +0530

    php final code

--- orderHistory.php
+++ orderHistory.php
@@ -1,116 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simseekr</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="./img/tg-icon.svg">
-    <link rel="stylesheet" href="uiframe/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.css">
-    <link rel="stylesheet" href="css/swiper-bundle.min.css">
-    <link rel="stylesheet" href="uiframe/css/slick.css">
-    <link rel="stylesheet" href="uiframe/css/slick-theme.css">
-    <link rel="stylesheet" href="css/t_styles.css">
-    <link rel="stylesheet" href="css/rstyle.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
-    <link rel="stylesheet" href="uiframe/css/font-awesome.min.css">
-    <link rel="stylesheet" href="uiframe/css/animate.min.css">
-    <link href="uiframe/css/aos.css" rel="stylesheet">
-</head>
-
-<body>
-
-    <div class="main-div">
-        <header class="header-top fixed-top" id="header-top">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    <a class="navbar-brand" href="#">
-                        <img src="./img/hlogo.svg" alt="Simseekr Logo" class="img-fluid">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav m-auto">
-                            <h5>Navigation</h5>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Shop eSIMs</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link " href="#">Support Hub</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link " href="#">Contact Us</a>
-                            </li>
-                        </ul>
-                        <ul class="navbar-nav m-auto menu-items-mobile">
-                            <h5>Navigation</h5>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Terms & Conditions</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Privacy Policy</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link " href="#">Cookies Policy</a>
-                            </li>
-                        </ul>
-                        <div class="d-flex right-side-menu">
-                            <ul class="navbar-nav">
-                                <li class="nav-item dropdown">
-                                    <button class="nav-link dropdown-toggle" id="currencyBtn" role="button"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                        $ USD <img src="./img/usa-flag.svg" class="img-fluid" id="currencyFlag">
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        <li><button class="dropdown-item"
-                                                onclick="selectCurrency('$ USD','./img/usa-flag.svg')">$ USD <img
-                                                    src="./img/usa-flag.svg" class="img-fluid"></button></li>
-                                        <li><button class="dropdown-item"
-                                                onclick="selectCurrency('$ CAD','./img/usa-flag.svg')">$ CAD <img
-                                                    src="./img/usa-flag.svg" class="img-fluid"></button></li>
-                                        <li><button class="dropdown-item"
-                                                onclick="selectCurrency('$ EUR','./img/france.svg')">$ EUR <img
-                                                    src="./img/france.svg" class="img-fluid"></button></li>
-                                        <li><button class="dropdown-item"
-                                                onclick="selectCurrency('$ GBP','./img/italy.svg')">$ GBP <img
-                                                    src="./img/italy.svg" class="img-fluid"></button></li>
-                                        <li><button class="dropdown-item"
-                                                onclick="selectCurrency('$ AUD','./img/thailand.svg')">$ AUD <img
-                                                    src="./img/thailand.svg" class="img-fluid"></button></li>
-                                        <li><button class="dropdown-item"
-                                                onclick="selectCurrency('$ NZD','./img/usa-flag.svg')">$ NZD <img
-                                                    src="./img/usa-flag.svg" class="img-fluid"></button></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link ">
-                                        <div class="cartCount">1</div>
-                                        <img src="./img/shopping-basket-solid.svg" alt="Shopping Basket Icon"
-                                            class="shopping-basket-icon img-fluid">
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <img src="./img/user-icon.svg" alt="Support Icon" class="user-icon img-fluid">
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="header-bottom d-block d-lg-none d-md-block">
-                            <p>© 2026 Simseekr. All rights reserved.</p>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
+<?php include 'includes/header.php'; ?>
 
         <Maindiv class="mas_main orderhistory_padd" >
             <div class="mas1">
@@ -355,134 +243,29 @@
 
 
 
+        <?php include 'includes/footer.php'; ?>
 
 
-        <footer class="footer">
-            <div class="container">
-                <div class="col">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-12 col-sm-12 col-12">
-                            <div class="footer-logo">
-                                <img src="./img/fLogo.svg" alt="Simseekr Logo" class="img-fluid">
-                                <p>© 2026 Simseekr. All rights reserved.</p>
-                                <div class="footer-p">
-                                    <ul>
-                                        <li>SimseekrSupport@email.co</li>
-                                        <li>+111 234 567 890</li>
-                                    </ul>
-                                    <p class="m-0">100 Edenbridge Road, Birmingham, United Kingdom, B28 8PR. </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-1 col-md-12 col-sm-12 col-12"></div>
-                        <div class="col-lg-7 col-md-12 col-sm-12 col-12">
-                            <div class="row-footer col-right-main-footer">
-                                <div class="col-right">
-                                    <div class="footer-link socials-link">
-                                        <h6>Socials</h6>
-                                        <ul>
-                                            <li>
-                                                <a href="#">
-                                                    <img src="./img/fb.svg" alt="Facebook">
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">
-                                                    <img src="./img/insta.svg" alt="Instagram">
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="col-right">
-                                    <div class="footer-link footer-navigation-link">
-                                        <h6>Navigation</h6>
-                                        <div class="footer-navigation">
-                                            <ul>
-                                                <li><a href="#">Shop eSIMs</a></li>
-                                                <li><a href="#">about us</a></li>
-                                                <li><a href="#">Login / Register</a></li>
-                                            </ul>
-                                            <ul>
-                                                <li><a href="#">Support Hub</a></li>
-                                                <li><a href="#">Contact us</a></li>
-                                                <li><a href="#">Supported Devices</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-right">
-                                    <div class="footer-link policies-link">
-                                        <h6>Policies</h6>
-                                        <ul>
-                                            <li>
-                                                <a href="#">Terms & conditions</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">privacy policy</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
-    <script src="uiframe/js/jquery.min.js"></script>
-    <script src="uiframe/js/bootstrap.bundle.min.js"></script>
-    <script src="uiframe/js/popper.min.js"></script>
-    <script src="uiframe/js/slick.js"></script>
-    <script src="uiframe/js/owl.carousel.js"></script>
-    <script src="uiframe/js/swiper-bundle.min.js"></script>
-    <script src="uiframe/js/flickity.pkgd.min.js"></script>
-    <script src="uiframe/js/aos.js"></script>
-    <script src="./uiframe/js/home-js.js"></script>
-    <script>
-        $(document).ready(function () {
-            $(".navbar-toggler").click(function () {
-                $(this).toggleClass("is-active");
-                $(".navbar-expand-lg").toggleClass("header-is-active");
-            });
-        });
-    </script>
-    <script>
-        function selectCurrency(currency, flagSrc) {
-            const btn = document.getElementById("currencyBtn");
-            const flag = document.getElementById("currencyFlag");
-            btn.innerHTML = `${currency} <img id="currencyFlag" src="${flagSrc}" class="img-fluid">`;
+
+
+
+
+
+
+
+
+
+<script>
+    function passwordEye(e, inpId) {
+        const input = document.querySelector(`#${inpId}`);
+        const img = e.target;
+        const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+        input.setAttribute('type', type);
+
+        if (img.src.includes("ic.svg")) {
+            img.src = img.src.replace("ic.svg", "io.svg");
+        } else {
+            img.src = img.src.replace("io.svg", "ic.svg");
         }
-    </script>
-    <script>
-        window.addEventListener('scroll', function () {
-            var content = document.querySelector('header');
-            var scrollPosition = window.scrollY;
-            if (scrollPosition > 10) {
-                content.classList.add("header-scroll")
-            } else {
-                content.classList.remove("header-scroll")
-            }
-        });
-    </script>
-    <script>
-        function passwordEye(e, inpId) {
-            const input = document.querySelector(`#${inpId}`);
-            const img = e.target;
-            const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
-            input.setAttribute('type', type);
-
-            if (img.src.includes("ic.svg")) {
-                img.src = img.src.replace("ic.svg", "io.svg");
-            } else {
-                img.src = img.src.replace("io.svg", "ic.svg");
-            }
-        }
-    </script>
-    <script>
-        AOS.init();
-    </script>
-</body>
-
-</html>
+    }
+</script>
